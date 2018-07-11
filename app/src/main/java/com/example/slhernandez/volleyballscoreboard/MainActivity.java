33e86a0f753db306507c4865730473f6031f9a9b
sergiohdez <sergioluisha@gmail.com>
2018-07-11 08:44:10 -0500

    Remove innecessary input

--- app/src/main/java/com/example/slhernandez/volleyballscoreboard/MainActivity.java
+++ app/src/main/java/com/example/slhernandez/volleyballscoreboard/MainActivity.java
@@ -12,7 +12,6 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
